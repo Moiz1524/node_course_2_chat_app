ae032122652c5ff389d06365afd15db641ccb339
Moiz Ali <amoiz4142@gmail.com>
2019-01-01 18:20:56 +0500

    Greet new User and alert others

--- public/js/index.js
+++ public/js/index.js
@@ -3,10 +3,10 @@ var socket = io();
 socket.on('connect', function () {
   console.log('Connected to server');
 
-  socket.emit('createMessage', {
-    from: 'Moiz',
-    text: 'It should work!.'
-  });
+  // socket.emit('createMessage', {
+  //   from: 'Moiz',
+  //   text: 'It should work!.'
+  // });
 });
 socket.on('disconnect', function () {
   console.log('Disconnected from server');
